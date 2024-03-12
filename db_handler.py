9c46973f120efa5e40649c117564446457dd4708
Mhyk Magdangal <mhykmagdangal19@gmail.com>
2024-03-12 22:32:05 +0800

    some changes

--- db_handler.py
+++ db_handler.py
@@ -3,7 +3,7 @@ import models
 
 class DBHandler:
     def __init__(self):
-        self.profile_db = 'databae.db'
+        self.profile_db = 'database.db'
         self.profile_table = 'profiles'
 
         self.conn = sqlite3.connect(self.profile_db)
